d7cad4e921e0fc8c974f7d1ccf142aedef29930b
riyansetiyadi <setiyariyan19@gmail.com>
2021-09-30 00:47:20 +0700

    perulangan array map

--- src/todo-list/TodoList.js
+++ src/todo-list/TodoList.js
@@ -2,7 +2,13 @@ import './TodoList.css';
 import React from 'react';
 
 const TodoList = (props) => {
-  return <ul>props.dataTodos.forEach(element => {})</ul>;
+  return (
+    <ul>
+      {props.dataTodos.map((todo) => {
+        return <li key={todo.id}>{todo.title}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default TodoList;
