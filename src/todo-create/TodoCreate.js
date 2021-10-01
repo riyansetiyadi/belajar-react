49934dd172310256e793482c34c67ef25f79665a
riyansetiyadi <setiyariyan19@gmail.com>
2021-10-02 05:50:33 +0700

    1 file modified, 2 added

---
+++ src/todo-create/TodoCreate.js
@@ -0,0 +1,28 @@
+import './TodoCreate.css';
+import React from 'react';
+import { useState } from 'react';
+
+const TodoCreate = (props) => {
+  const [getInputTodo, setInputTodo] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: Math.floor(Math.random() * 100) + 1,
+      title: getInputTodo,
+    };
+    props.onCreateTodo(newTodo);
+    setInputTodo('');
+  };
+  const handleInputTodo = (event) => {
+    setInputTodo(event.target.value);
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input type="text" value={getInputTodo} onChange={handleInputTodo} />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default TodoCreate;
--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,18 +1,25 @@
 import './Todo.css';
 import TodoList from '../todo-list/TodoList';
 import React from 'react';
+import TodoCreate from '../todo-create/TodoCreate';
+import { useState } from 'react';
 
 const todo = () => {
-  const todos = [
+  const [getTodos, setTodos] = useState([
     { id: 1, title: 'Eat' },
     { id: 2, title: 'Sleep' },
     { id: 3, title: 'Code' },
-  ];
+  ]);
+
+  const eventCreateTodo = (todo) => {
+    setTodos(getTodos.concat(todo));
+  };
 
   return (
     <div>
       <h3>Todo List</h3>
-      <TodoList dataTodos={todos} />
+      <TodoCreate onCreateTodo={eventCreateTodo} />
+      <TodoList dataTodos={getTodos} />
     </div>
   );
 };
